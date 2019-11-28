dedbad12cfdfcce51eb61ecb3f9bb72168160e2b
chagen <Christer.hagen@gmail.com>
2019-11-28 21:06:03 +0100

    noe feil

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Christer:Ei934ei9@cluster0-bjxdg.mongodb.net/test?retryWrites=true&w=majority"
+};
+
+module.exports = {
+    mongoURI: "mongodb+srv://Christer:Ei934ei9@cluster0-bjxdg.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
